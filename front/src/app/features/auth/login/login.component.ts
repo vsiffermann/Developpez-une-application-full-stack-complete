09daf4ea6d2fb465ca96fd6f72435b42e3b16f35
vsiffermann <vsiffermann@divalto.com>
2026-04-29 23:22:32 +0200

    front: implement login form

--- front/src/app/features/auth/login/login.component.ts
+++ front/src/app/features/auth/login/login.component.ts
@@ -1,23 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+import { Store } from '@ngrx/store';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { MatIcon } from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { AuthActions } from '../../../store/auth/auth.actions';
+import { selectAuthError, selectAuthLoading } from '../../../store/auth/auth.selectors';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
+    ReactiveFormsModule,
     RouterLink,
+    AsyncPipe,
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatIcon,
+    MatIconModule,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  private readonly store = inject(Store);
+
+  readonly loading$ = this.store.select(selectAuthLoading);
+  readonly error$ = this.store.select(selectAuthError);
+  readonly showPassword = signal(false);
+
+  readonly form = new FormGroup({
+    identifier: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  onSubmit(): void {
+    if (this.form.invalid) return;
+    const { identifier, password } = this.form.value;
+    this.store.dispatch(AuthActions.login({ identifier: identifier!, password: password! }));
+  }
+}
