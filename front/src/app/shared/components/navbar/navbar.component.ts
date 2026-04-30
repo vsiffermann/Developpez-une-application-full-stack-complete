f64f2b3328d17df240215006d06cf729f0fa3434
vsiffermann <vsiffermann@divalto.com>
2026-04-30 16:41:19 +0200

    front : redesign navbar to match figma template

--- front/src/app/shared/components/navbar/navbar.component.ts
+++ front/src/app/shared/components/navbar/navbar.component.ts
@@ -4,15 +4,24 @@ import { Store } from '@ngrx/store';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatMenuModule } from '@angular/material/menu';
+import { LogoComponent } from '../logo/logo.component';
 import { AuthActions } from '../../../store/auth/auth.actions';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, MatToolbarModule, MatButtonModule, MatIconModule, MatTooltipModule],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    LogoComponent,
+  ],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss',
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
   private readonly store = inject(Store);
